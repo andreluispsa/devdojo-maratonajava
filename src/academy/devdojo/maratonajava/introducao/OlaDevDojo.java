251a1e8a79c644263a1636747090c8f33b57679f
Andre Sa <andrepena.sa@gmail.com>
2021-08-21 00:13:31 -0300

    Alteração do Hello World

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OlaDevDojo {
+
+    /**
+     Isso é um comentário do tipo <b>javadoc</b>.
+     */
+
+    // Isso é um comentário em linha
+
+    /*
+        Isso é um comentário
+        de múltiplas
+        linhas
+    */
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+    }
+}
